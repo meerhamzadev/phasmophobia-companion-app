1aaca8de8e07c74ca4e7bbec34c63798b1631f4c
Rodrigo <rodrigo.m.castilho@outlook.com>
2020-12-12 20:24:51 -0300

    Creating first structure

---
+++ src/components/EvidenceContainer/EvidenceContainer.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function EvidenceContainer(props){
+
+
+    return(
+        <div className={"evidence-container"}>
+            <h2>With the evidence below...</h2>
+        </div>
+    )
+}
