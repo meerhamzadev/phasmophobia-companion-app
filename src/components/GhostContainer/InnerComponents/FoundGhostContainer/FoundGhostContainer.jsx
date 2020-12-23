1e9dea9ff10130763e4b60c7ce12680d11c5899a
Rodrigo <rodrigo.m.castilho@outlook.com>
2020-12-23 12:27:18 -0300

    Add better version of found ghost container

--- src/components/GhostContainer/InnerComponents/FoundGhostContainer/FoundGhostContainer.jsx
+++ src/components/GhostContainer/InnerComponents/FoundGhostContainer/FoundGhostContainer.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import GhostCard from "../GhostCard/GhostCard";
+import './FoundGhostContainer.css'
 
 export default function FoundGhostContainer(props){
+
+    const foundGhost = props.foundGhost[0]
+
     return(
         <div className={"found-ghost-container"}>
             <div className={"found-ghost-card"}>
-                <GhostCard cardType={'found'} ghostType={props.foundGhost.type} ghostId={props.foundGhost.id} />
+                <GhostCard cardType={'found'} ghostType={foundGhost.type} ghostId={foundGhost.id} />
             </div>
             <div className={"found-ghost-description"}>
-                <h4>{props.foundGhost.description}</h4>
-                <ul>
-                    <li>Strength: {props.foundGhost.strength}</li>
-                    <li>Weakness: {props.foundGhost.weakness}</li>
-                </ul>
+                <div className={"found-ghost-inner-text"}>
+                    <h4>{foundGhost.description}</h4>
+                    <ul>
+                        <li><b>Strength:</b> {foundGhost.strength}</li>
+                        <p/>
+                        <li><b>Weakness:</b> {foundGhost.weakness}</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
