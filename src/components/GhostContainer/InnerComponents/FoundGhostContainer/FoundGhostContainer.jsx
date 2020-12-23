895080934e4b316e74d421940e4f81c146c79666
Rodrigo <rodrigo.m.castilho@outlook.com>
2020-12-23 11:24:41 -0300

    Start refactor for found ghost card

--- src/components/GhostContainer/InnerComponents/FoundGhostContainer/FoundGhostContainer.jsx
+++ src/components/GhostContainer/InnerComponents/FoundGhostContainer/FoundGhostContainer.jsx
@@ -4,13 +4,15 @@ import GhostCard from "../GhostCard/GhostCard";
 export default function FoundGhostContainer(props){
     return(
         <div className={"found-ghost-container"}>
-            <div className={"found-ghost-picture"}>
-
+            <div className={"found-ghost-card"}>
+                <GhostCard cardType={'found'} ghostType={props.foundGhost.type} ghostId={props.foundGhost.id} />
             </div>
             <div className={"found-ghost-description"}>
-                <h2>{props.message}</h2>
-
-                {props.ghostDescription}
+                <h4>{props.foundGhost.description}</h4>
+                <ul>
+                    <li>Strength: {props.foundGhost.strength}</li>
+                    <li>Weakness: {props.foundGhost.weakness}</li>
+                </ul>
             </div>
         </div>
     )
