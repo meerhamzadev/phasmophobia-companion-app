444c90305a38c26e75d30abf1cb8efec45bb8d01
MeerHamza1421 <hamzababar37@gmail.com>
2021-01-05 01:51:18 +0500

    Done tablet Responsive Part

--- src/components/GhostContainer/InnerComponents/PossibleGhostsContainer/PossibleGhostContainer.jsx
+++ src/components/GhostContainer/InnerComponents/PossibleGhostsContainer/PossibleGhostContainer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import GhostCard from "../GhostCard/GhostCard";
+import './PossibleGhostContainer.css';
 
-export default function PossibleGhostContainer(props){
+export default function PossibleGhostContainer(props) {
     const ghostToRender = props.possibleGhosts.map((ghost) => {
         return <GhostCard cardType={'possible'} ghostType={ghost.type} ghostId={ghost.id} />
     });
 
-    return(
+    return (
         <div className={'possible-ghosts-container'}>
             {ghostToRender}
         </div>
