d0a2d56395fbd363783dd02f7ff54f76cbd4635c
Rodrigo <rodrigo.m.castilho@outlook.com>
2021-01-05 13:47:14 -0300

    New CSS corrections for switch displays

--- src/components/EvidenceContainer/InnerComponents/ResetButton/ResetButton.jsx
+++ src/components/EvidenceContainer/InnerComponents/ResetButton/ResetButton.jsx
@@ -3,6 +3,8 @@ import './ResetButton.css'
 
 export default function ResetButton(props){
     return(
-        <button className={'reset-button'} onClick={props.resetEvidence}>Reset evidence</button>
+        <div className={"evidence-controls"}>
+            <button className={'reset-button'} onClick={props.resetEvidence}>Reset evidence</button>
+        </div>
     )
 }
