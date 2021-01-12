928422139d3bf7cab890d4bbd04b87eefae08482
Rodrigo <rodrigo.m.castilho@outlook.com>
2021-01-12 09:40:59 -0300

    [#16] Fix whiteboard rendering

--- src/components/UtilitiesContainer/InnerComponents/UtilitiesTabs.jsx
+++ src/components/UtilitiesContainer/InnerComponents/UtilitiesTabs.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 export default function UtilitiesTabs(props){
+    const evidenceClass = props.currentUtility === "evidence" ? "active-utility-tab" : "utility-tab";
+    const timerClass = props.currentUtility === "timer" ? "active-utility-tab" : "utility-tab";
+    const boardClass = props.currentUtility === "board" ? "active-utility-tab" : "utility-tab";
+
     return(
         <div className={"tabs-container"}>
-            <button className={"utility-tab"} value={"evidence"} onClick={props.handleUtility}>Evidence</button>
-            <button className={"utility-tab"} value={"timer"} onClick={props.handleUtility}>Preparation Time</button>
-            <button className={"utility-tab"} value={"board"} onClick={props.handleUtility}>Whiteboard</button>
+            <button className={evidenceClass} value={"evidence"} onClick={props.handleUtility}>Evidence</button>
+            <button className={timerClass} value={"timer"} onClick={props.handleUtility}>Preparation Time</button>
+            <button className={boardClass} value={"board"} onClick={props.handleUtility}>Whiteboard</button>
         </div>
-
     )
 }
--- src/components/UtilitiesContainer/InnerComponents/Whiteboard/Whiteboard.jsx
+++ src/components/UtilitiesContainer/InnerComponents/Whiteboard/Whiteboard.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 
 export default function Whiteboard(props){
-
-    const dataList = (
-        <datalist>
-            <option value={"Have a member of your team witness a Ghost Event"} />
-            <option value={"Capture a photo of the Ghost"} />
-            <option value={"Capture a photo of Dirty Water in a sink"} />
-            <option value={"Find evidence of the paranormal with an EMF Reader"} />
-            <option value={"Detect a room below 10 Celsius/50 Fahrenheit with a Thermometer"} />
-            <option value={"Detect a Ghosts presence with a Motion Sensor"} />
-            <option value={"Cleanse the area near the Ghost using Smudge Sticks"} />
-            <option value={"Prevent the Ghost from hunting with a Crucifix"} />
-            <option value={"Get a Ghost to walk through Salt"} />
-        </datalist>
-    );
-
     return(
         <div className={'white-board'}>
             <span className={"ghost-name-label"}>Ghost name:</span> <input className={"ghost-name"}/>
             <h2 className={"board-title"}>Session objectives</h2>
             <span className={"objective"}>Objective 1:</span> <span className={"first-objective"}>Discover what type of Ghost we are dealing with</span>
-            <span className={"objective"}>Objective 2:</span> <input id="objective-two" name="objective-two" list={dataList} />
-            <span className={"objective"}>Objective 3:</span> <input id="objective-three" name="objective-three" list={dataList} />
-            <span className={"objective"}>Objective 4:</span> <input id="objective-four" name="objective-four" list={dataList} />
+            <span className={"objective"}>Objective 2:</span> <input type={"text"} id="objective-two" list={"objective-data"} />
+            <span className={"objective"}>Objective 3:</span> <input type={"text"} id="objective-three" list={"objective-data"} />
+            <span className={"objective"}>Objective 4:</span> <input type={"text"} id="objective-four" list={"objective-data"} />
+
+            <datalist id={"objective-data"}>
+                <option value={"Have a member of your team witness a Ghost Event"} />
+                <option value={"Capture a photo of the Ghost"} />
+                <option value={"Capture a photo of Dirty Water in a sink"} />
+                <option value={"Find evidence of the paranormal with an EMF Reader"} />
+                <option value={"Detect a room below 10 Celsius/50 Fahrenheit with a Thermometer"} />
+                <option value={"Detect a Ghosts presence with a Motion Sensor"} />
+                <option value={"Cleanse the area near the Ghost using Smudge Sticks"} />
+                <option value={"Prevent the Ghost from hunting with a Crucifix"} />
+                <option value={"Get a Ghost to walk through Salt"} />
+            </datalist>
+
             <div className={"ghost-behavior-container"}>
                 <span className={"ghost-behavior-label"}>Ghost responds to</span>
-                <input type={"radio"} className={"ghost-behavior-radio"} name={"behavior"}>Individuals</input>
-                <input type={"radio"} className={"ghost-behavior-radio"} name={"behavior"}>Groups</input>
+
+                <input type={"radio"} className={"ghost-behavior-radio"} id="individuals" name={"behavior"} value={"individuals"} />
+                <label>Individuals</label>
+
+                <input type={"radio"} className={"ghost-behavior-radio"} id="groups" name={"behavior"} value={"groups"} />
+                <label>Groups</label>
             </div>
         </div>
     )
