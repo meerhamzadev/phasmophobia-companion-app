a5444ff0bc972712a145906c80dfce40f457f3b8
Rodrigo <rodrigo.m.castilho@outlook.com>
2021-04-26 16:52:14 -0300

    [#43] Add more structure to the map reference

--- src/components/MapReference/InnerComponents/InnerComponents/MapContent/InnerComponents/PhotoGallery/PhotoGallery.jsx
+++ src/components/MapReference/InnerComponents/InnerComponents/MapContent/InnerComponents/PhotoGallery/PhotoGallery.jsx
@@ -11,17 +11,24 @@ import ridgeviewLabels from '../../../../../../../images/maps/ridgeview/map_labe
 import tanglewoodLabels from '../../../../../../../images/maps/tanglewood/map_labels.jpg';
 
 // Map entrances
-
+import asylumEntrance from '../../../../../../../images/maps/asylum/asylum-outside.jpg';
+import bleasdaleEntrance from '../../../../../../../images/maps/bleasdale/bleasdale-outside.jpg';
+import edgefieldEntrance from '../../../../../../../images/maps/edgefield/edgefield-outside.jpg';
+import graftonEntrance from '../../../../../../../images/maps/grafton/grafton-outside.jpg';
+import highschoolEntrance from '../../../../../../../images/maps/highschool/highschool-outside.jpg';
+import prisonEntrance from '../../../../../../../images/maps/prison/prison-outside.jpg';
+import ridgeviewEntrance from '../../../../../../../images/maps/ridgeview/ridgeview-outside.jpg';
+import tanglewoodEntrance from '../../../../../../../images/maps/tanglewood/tanglewood-outside.jpg';
 
 export default function PhotoGallery(props){
     const photoReferences = {
-        'asylum': [asylumLabels],
-        'bleasdale': [bleasdaleLabels],
-        'edgefield': [edgefieldLabels],
-        'grafton': [graftonLabels],
-        'highschool': [highschoolLabels],
-        'prison': [prisonLabels],
-        'ridgeview': [ridgeviewLabels],
-        'tanglewood': [tanglewoodLabels]
+        'asylum': [asylumLabels, asylumEntrance],
+        'bleasdale': [bleasdaleLabels, bleasdaleEntrance],
+        'edgefield': [edgefieldLabels, edgefieldEntrance],
+        'grafton': [graftonLabels, graftonEntrance],
+        'highschool': [highschoolLabels, highschoolEntrance],
+        'prison': [prisonLabels, prisonEntrance],
+        'ridgeview': [ridgeviewLabels, ridgeviewEntrance],
+        'tanglewood': [tanglewoodLabels, tanglewoodEntrance]
     }
 }
